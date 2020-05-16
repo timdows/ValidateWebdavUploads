fca569902d5137c60bc7b720f7c19bca26ac4b8d
timdows <tim@inexpro.nl>
2020-05-16 16:58:54 +0200

    Add console app

---
+++ ValidateWebdavUploads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidateWebdavUploads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
